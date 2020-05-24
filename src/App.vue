ad6d487a52cd9c51395c6f73a5e545dcb0b563c1
jyoti attri <jyotiatri22@gmail.com>
2020-05-24 12:46:22 +0530

    numbers shown in output

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ParentComponent/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ParentComponent from "./components/ParentComponent";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    ParentComponent
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,4 +25,3 @@ export default {
   margin-top: 60px;
 }
 </style>
-
