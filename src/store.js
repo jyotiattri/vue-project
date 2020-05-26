61741bb086a49c80327cdb0de7e751c8cc46b942
jyoti attri <jyotiatri22@gmail.com>
2020-05-26 14:42:33 +0530

    vuex added

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-export const store = {
+import Vue from 'vue';
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
     state: {
         numbers: [1, 2, 3]
     },
     addNumber(newNumber) {
         this.state.numbers.push(newNumber);
     }
-};
+});
